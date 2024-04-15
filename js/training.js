0fbfb774ad1f565451c266eae4b9127af6ff48de
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-15 15:00:16 +0200

    exercice 2 done

--- js/training.js
+++ js/training.js
@@ -1,22 +1,30 @@
 /* ------------------------------------ */
 /* --- Exercice 1 --- */
 
-document.querySelector("#button1").addEventListener('click', function(event){
+document.querySelector("#button1").addEventListener('click', function (event) {
     document.querySelector("#text1").classList.toggle('text-hidden')
 })
 
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
 
-let y = window.scrollY;
 let exercice2 = document.querySelector("#ex2-scroll-value");
 console.log(exercice2);
 
-    exercice2.innerHTML = y;
+
+document.addEventListener('scroll', function(event){
+    exercice2.innerText = Math.floor(window.scrollY);
+})
 
 /* ------------------------------------ */
 /* --- Exercice 3 --- */
 
+document.querySelector("#button3").addEventListener('click', function (event) {
+    document.querySelector("#ex3-animals")
+})
+
+console.log(document.querySelector("#button3"));
+console.log(document.querySelector("#ex3-animals"));
 
 /* ------------------------------------ */
 /* --- Exercice 4 --- */
