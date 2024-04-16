e6ca447719cea496d2c15619452d43a8c35ed209
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-16 11:58:52 +0200

    exercice 4 fucked and exercice 5 in progress. First button changes

--- js/training.js
+++ js/training.js
@@ -8,7 +8,7 @@ document.querySelector("#button1").addEventListener('click', function (event) {
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
 
-document.addEventListener('scroll', function(event){
+document.addEventListener('scroll', function (event) {
     document.querySelector("#ex2-scroll-value").innerText = Math.floor(window.scrollY);
 })
 
@@ -22,10 +22,38 @@ document.querySelector("#button3").addEventListener('click', function (event) {
 /* ------------------------------------ */
 /* --- Exercice 4 --- */
 
-
+document.querySelector("#button4").addEventListener('click', function (event) {
+    let clickCounter = 0;
+    clickCounter += 1;
+    if (clickCounter === 1) {
+        document.querySelector("#button4").classList.add('btn--blue')
+    }
+    if (clickCounter === 2) {
+        document.querySelector("#button4").classList.add('btn--red')
+    }
+    if (clickCounter === 3) {
+        document.querySelector("#button4").classList.add('btn--green')
+    }
+});
 
 /* ------------------------------------ */
 /* --- Exercice 5 --- */
+
+// mouseover
+
+document.querySelectorAll("#mouse-over-exercice li .js-btn")
+
+    for (const index of document.querySelectorAll("#mouse-over-exercice li .js-btn")) {
+        addEventListener('mouseover', function (event){
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            let rgbValue = "#" + randomColor;
+            document.querySelector("#mouse-over-exercice li .js-btn").style.backgroundColor = rgbValue;
+    })
+};
+
+console.log(document.querySelectorAll("#mouse-over-exercice li .js-btn"));
+
+console.log(document.querySelector("#mouse-over-exercice li .js-btn"));
 
 
 /* ------------------------------------ */
