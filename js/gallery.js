572630e9aee69b43d1462581afd72dd517953070
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-18 09:46:18 +0200

    the mouseover works on every project but I simply repeated my code for every project. I must find a way to automatize it. I also created an array of objects containing every project datas to do so

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,106 @@
+const projects = [
+    {
+        name: document.querySelector('[alt="Projet 1"]').alt,
+        picture: "img/project-1",
+        description: document.getElementById('project-1').dataset['description'],
+        title: document.getElementById('project-2').dataset['title']
+    },
+    {
+        title: document.querySelector('[alt="Projet 2"]').alt,
+        picture: "img/project-2",
+        description: document.getElementById('project-2').dataset['description'],
+        title: document.getElementById('project-2').dataset['title']
+    },
+    {
+        title: document.querySelector('[alt="Projet 3"]').alt,
+        picture: "img/project-3",
+        description: document.getElementById('project-3').dataset['description'],
+        title: document.getElementById('project-2').dataset['title']
 
+    },
+    {
+        title: document.querySelector('[alt="Projet 4"]').alt,
+        picture: "img/project-4",
+        description: document.getElementById('project-4').dataset['description'],
+        title: document.getElementById('project-2').dataset['title']
+
+    },
+    {
+        title: document.querySelector('[alt="Projet 5"]').alt,
+        picture: "img/project-5",
+        description: document.getElementById('project-5').dataset['description'],
+        title: document.getElementById('project-2').dataset['title']
+
+    },
+    {
+        title: document.querySelector('[alt="Projet 6"]').alt,
+        picture: "img/project-6",
+        description: document.getElementById('project-6').dataset['description'],
+        title: document.getElementById('project-2').dataset['title']
+    },
+]
+
+console.log(document.getElementById('project-2').dataset['title']);
+console.log(document.getElementById('project-2').dataset['description']);
+
+
+
+const project1 = document.querySelector('[data-title="Consectetur adipiscing elit"]');
 const project2 = document.querySelector('[data-title="Voluptatem accusantium"]');
+const project3 = document.querySelector('[data-title="Denouncing pleasure and praising pain"]');
+const project4 = document.querySelector('[data-title="Ducimus qui blanditiis praesentium"]');
+const project5 = document.querySelector('[data-title="Pleasure and pain"]');
+const project6 = document.querySelector('[data-title="Focus event on form elements"]');
 
 const image = document.getElementById('gallery-picture');
 
-let description = document.getElementById('gallery-description')
+const description = document.getElementById('project-2').dataset['description'];
 
 console.log(image);
 
 // console.log(project2);
 // console.log(document.querySelector('[alt="Projet 2"]').alt);
 
+project1.addEventListener('mouseover', function () {
+    document.getElementById('gallery-title').textContent = document.querySelector('[alt="Projet 1"]').alt;
+    image.src = "img/project-1.jpg";
+    description.textContent = document.getElementById('project-1').dataset['description'];
+});
+
 project2.addEventListener('mouseover', function () {
     document.getElementById('gallery-title').textContent = document.querySelector('[alt="Projet 2"]').alt;
     image.src = "img/project-2.jpg";
-    description = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+    description.textContent = document.getElementById('project-2').dataset['description'];
 });
+
+project3.addEventListener('mouseover', function () {
+    document.getElementById('gallery-title').textContent = document.querySelector('[alt="Projet 3"]').alt;
+    image.src = "img/project-3.jpg";
+    description.textContent = document.getElementById('project-3').dataset['description'];
+});
+
+project4.addEventListener('mouseover', function () {
+    document.getElementById('gallery-title').textContent = document.querySelector('[alt="Projet 4"]').alt;
+    image.src = "img/project-4.jpg";
+    description.textContent = document.getElementById('project-4').dataset['description'];
+});
+
+project5.addEventListener('mouseover', function () {
+    document.getElementById('gallery-title').textContent = document.querySelector('[alt="Projet 5"]').alt;
+    image.src = "img/project-5.jpg";
+    description.textContent = document.getElementById('project-5').dataset['description'];
+});
+
+project6.addEventListener('mouseover', function () {
+    document.getElementById('gallery-title').textContent = document.querySelector('[alt="Projet 6"]').alt;
+    image.src = "img/project-6.jpg";
+    description.textContent = document.getElementById('project-6').dataset['description'];
+});
+
+
+
+
+
+
+
+
