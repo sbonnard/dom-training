fe997aa64acb33f828ced99e05fca421bf9b8e87
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-18 12:23:15 +0200

    Does not work when I try to generalize it

--- js/tabs.js
+++ js/tabs.js
@@ -1,8 +1,30 @@
-function addActiveClass() { 
-    document.getElementById('tab2').classList.add("active-tab"); 
-}
+const tab1 = document.getElementById('tab1');
+const tab2 = document.getElementById('tab2');
+const tab3 = document.getElementById('tab3');
+const tab4 = document.getElementById('tab4');
 
-document.getElementById('tab2').addEventListener('click', function(event){addActiveClass()}
+
+    tab2.classList.add("active-tab");
+
+
+tab2.addEventListener('click', function (event) {
+    tab2.classList.add("active-tab");;
+    if (tab1.classList.contains('active-tab') || tab3.classList.contains('active-tab') || tab4.classList.contains('active-tab')) {
+        tab1.classList.remove('active-tab');
+        tab2.classList.remove('active-tab');
+        tab3.classList.remove('active-tab');
+    }
+}
+)
+
+tab1.addEventListener('click', function (event) {
+    tab1.classList.add("active-tab");
+    if (tab2.classList.contains('active-tab') || tab3.classList.contains('active-tab') || tab4.classList.contains('active-tab')) {
+        tab2.classList.remove('active-tab');
+        tab3.classList.remove('active-tab');
+        tab4.classList.remove('active-tab');
+    }
+}
 )
 
 console.log(document.getElementById('tab2'));
